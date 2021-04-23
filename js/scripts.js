2b0ff525e2a02567b70b644d7c812b0b28d7beef
Wairimu-Miano <wairimu.miano@student.moringaschool.com>
2021-04-23 23:53:07 +0300

    Functions and objects to compute total cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,119 @@
-//ORDER DETAILS
 
 //BACK END LOGIC
-function Pizza(delivery){
-  this.orders=[];
-  this.delivery=delivery;
-};
-function Order(size,crust,flavor,toppings,quantity){
+function Pizza(size,crust,flavor,toppings,quantity,delivery){
   this.size=size;
   this.crust=crust;
   this.flavor=flavor;
   this.toppings=toppings;
-  this.quantity=quantity;
-}
-Order.prototype.fullOrder=function(){
-  return this.size + ", " + this.crust + ", " + this.flavor + ", " + this.toppings + ", " + this.quantity;
-}
+  this.quantity=quantity
+  this.delivery=delivery;
+};
 var sizes={
   Small:500,
   Medium:800,
   Large:1000
 };
-var toppings={
-  topping1:50,
-  topping2:50,
-  topping3:50,
-  topping4:50,
-  topping5:50,
-  topping6:100,
-  topping7:100,
-  topping8:100,
-  topping9:100,
-  topping10:100,
-  topping11:100,
-  topping12:100,
-  topping13:100,
-  topping14:100,
-  topping15:100,
+// var toppingsCheap={
+//   topping:["Onions","Diced Tomatoes","Black OLives"]
+//   prices:50
+// }
+// var toppingsCheap={
+//   name:['Onions','Diced Tomatoes',"Mixed Peppers","Black Olives","Pineapples"],
+//   price:50
+// }
+// var toppingsExpensive={
+//   name:["Extra Cheese","Fresh Mushrooms","Steak","Grilled Chicken","Italian Sausages","Beef","Beef Ham","Jalapeno Peppers","Beef Bacon","Pepperoni"],
+//   price:100
+// }
+// var toppingsPrices={
+//   name:toppings,
+//   list:[toppingsCheap,toppingsExpensive]
+// }
+var toppingsPrices={
+  "Onions":50,
+  "Diced Tomatoes":50,
+  "Mixed Peppers":50,
+  "Black OLives":50,
+  "Pineapples":50,
+  "Extra Cheese":100,
+  "Fresh Mushrooms":100,
+  "Steak":100,
+  "Grilled Chicken":100,
+  "Italian Sausages":100,
+  "Beef":100,
+  "Beef Ham":100,
+  "Jalapeno Peppers":100,
+  "Beef Bacon":100,
+  "Pepperoni":100,
 }
+var deliveryCost=100;
 //USER INTERFACE
 $(document).ready(function(){
-  $("#submit").click(function(event){
+  $("form.pizza-old").submit(function(event){
+
     event.preventDefault();
-    var choseDelivery=$("input:radio[name=delivery]:checked").val();
-    var newPizza=new Pizza(choseDelivery)
+
     $(".new-pizza").each(function(){
       var inputtedSize=$("input:radio[name=size]:checked").val();
       var inputtedCrust=$(".crust-type").val();
       var inputtedFlavor=$(".flavor-type").val();
+
       var inputtedToppings=new Array();
       $("input[name=toppings]:checked").each(function(){
         inputtedToppings.push(this.value);
       });
+
       var inputtedQuantity=$("#number-pizzas").val();
-      var newOrder=new Order(inputtedSize,inputtedCrust,inputtedFlavor,inputtedToppings,inputtedQuantity);
-      newPizza.orders.push(newOrder);
-      newPizza.orders.forEach(function(order){
-        $("ul#order-details").append("<li>PIZZA SIZE: " +order.size+"<br>PIZZA CRUST: " + order.crust + "<br>PIZZA FLAVOR " + order.flavor + "<br>PIZZA TOPPINGS: " + order.toppings + "<br>NUMBER OF PIZZAS: " +order.quantity + "</li>" )
+      var choseDelivery=$("input:radio[name=delivery]:checked").val();
+
+      var newPizza=new Pizza(inputtedSize,inputtedCrust,inputtedFlavor,inputtedToppings,inputtedQuantity,choseDelivery)
+      var toppingsCost=0;
+      inputtedToppings.forEach(function(inputtedTopping){
+        toppingsCost+=parseInt(toppingsPrices.inputtedTopping)
       });
+      console.log(toppingsCost)
+      // for (var index=0;index<inputtedToppings.length;index+=1){
+      //   toppingsCost+=toppingsPrices.["inputtedToppings"]
+      //   return toppingsCost;
+      // }
+      if (choseDelivery==="YES"){
+        var pizzaCost= sizes.inputtedSize*inputtedQuantity + deliveryCost + toppingsCost*inputtedQuantity;
+      } else {
+        var pizzaCost= sizes.inputtedSize + toppingsCost;
+      }
+      $("#order-details").show();
+      $("span#order-size").text(newPizza.size);
+      $("span#order-crust").text(newPizza.crust);
+      $("span#order-flavor").text(newPizza.flavor);
+      $("span#order-toppings").text(newPizza.toppings);
+      $("span#order-quantity").text(newPizza.quantity);
+      $("#cost").text(pizzaCost);
     });
+
   })
 })
+
+
+
+// var sizes={
+//   Small:500,
+//   Medium:800,
+//   Large:1000
+// };
+// var toppings={
+//   topping1:50,
+//   topping2:50,
+//   topping3:50,
+//   topping4:50,
+//   topping5:50,
+//   topping6:100,
+//   topping7:100,
+//   topping8:100,
+//   topping9:100,
+//   topping10:100,
+//   topping11:100,
+//   topping12:100,
+//   topping13:100,
+//   topping14:100,
+//   topping15:100,
+// }

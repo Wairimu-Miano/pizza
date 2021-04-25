4ed15d997ff26cec024f25cf9aea1455de9c299f
Wairimu-Miano <wairimu.miano@student.moringaschool.com>
2021-04-25 11:36:16 +0300

    Remove toppingsPrices, edit method to check pizza price by size, add KSH to costs

--- js/scripts.js
+++ js/scripts.js
@@ -12,28 +12,13 @@ var sizes={
   small:500,
   medium:800,
   large:1000
-};
+}
 function Location(street,constituency,area){
   this.street=street;
   this.constituency=constituency;
   this.area=area;
 }
-// var toppingsCheap={
-//   topping:["Onions","Diced Tomatoes","Black OLives"]
-//   prices:50
-// }
-// var toppingsCheap={
-//   name:['Onions','Diced Tomatoes',"Mixed Peppers","Black Olives","Pineapples"],
-//   price:50
-// }
-// var toppingsExpensive={
-//   name:["Extra Cheese","Fresh Mushrooms","Steak","Grilled Chicken","Italian Sausages","Beef","Beef Ham","Jalapeno Peppers","Beef Bacon","Pepperoni"],
-//   price:100
-// }
-// var toppingsPrices={
-//   name:toppings,
-//   list:[toppingsCheap,toppingsExpensive]
-// }
+
 var toppingsCheap={
   name:['Onions','Diced Tomatoes','Mixed Peppers','Black Olives','Pineapples'],
   price:50
@@ -46,7 +31,6 @@ var deliveryCost=100;
 //USER INTERFACE
 $(document).ready(function(){
   $("form.pizza-old").submit(function(event){
-
     event.preventDefault();
 
     $(".new-pizza").each(function(){
@@ -73,47 +57,33 @@ $(document).ready(function(){
           var cost=0;
           toppingsCost+=cost
         }
-
       });
+
       if (choseDelivery==="delivery-yes"){
         var inputtedStreet=$("#street").val();
         var inputtedConstituency=$("#constituency").val();
         var inputtedArea=$("#area").val()
         var newLocation=new Location(inputtedStreet,inputtedConstituency,inputtedArea)
         $("span#delivery-location").text(newLocation.area)
-        var pizzaCost= sizes.inputtedSize*inputtedQuantity + deliveryCost + toppingsCost*inputtedQuantity;
-        $("span#total-cost").text(pizzaCost);
-        $("#cost").text(pizzaCost);
-
+        var pizzaCost= sizes[inputtedSize]*inputtedQuantity + deliveryCost + toppingsCost*inputtedQuantity;
+        $("span#total-cost").text("KSH. " + pizzaCost);
+        $("#cost").text("KSH." + pizzaCost);
       } else {
         $("span#delivery-location").text("None_Pick-Up")
-        var pizzaCost= sizes.inputtedSize*inputtedQuantity + toppingsCost*inputtedQuantity;
+        var pizzaCost= sizes[inputtedSize]*inputtedQuantity + toppingsCost*inputtedQuantity;
         $("span#total-cost").text(pizzaCost);
-        $("#cost").text(pizzaCost);
+        $("#cost").text("KSH. " + pizzaCost);
       }
       $(".order-details").show();
       $("span#order-size").text(newPizza.size);
       $("span#order-crust").text(newPizza.crust);
       $("span#order-flavor").text(newPizza.flavor);
-      $("span#order-toppings").text(newPizza.toppings);
+      if (newPizza.toppings.length===0){
+        $("span#order-toppings").text("None")
+      } else{
+        $("span#order-toppings").text(newPizza.toppings)
+      }
       $("span#order-quantity").text(newPizza.quantity);
-
-
     });
-
-  })
-})
-
-
-
-// var sizes={
-//   Small:500,
-//   Medium:800,
-//   Large:1000
-// };
-
-
-// for (var index=0;index<inputtedToppings.length;index+=1){
-//   toppingsCost+=toppingsPrices["inputtedToppings"]
-// }
-// console.log(toppingsCost)
+  });
+});
